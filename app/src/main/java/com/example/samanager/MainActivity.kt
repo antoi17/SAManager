1588ab618da5235571c9ee6319b78ab3b7a7e998
antoi17 <antoine.bonin986@gmail.com>
2021-11-14 18:55:06 +0100

    Tablayout nettoyé + list_view invisible

--- app/src/main/java/com/example/samanager/MainActivity.kt
+++ app/src/main/java/com/example/samanager/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +19,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView = findViewById<ListView>(R.id.list_view)
+        val listView = findViewById<ListView>(R.id.serie_list_view)
+        val tabs = findViewById<TabLayout>(R.id.Tabs)
+        val serietab = findViewById<TabItem>(R.id.serieTab)
+        val scantab = findViewById<TabItem>(R.id.scanTab)
 
         listView.adapter = DataAdapter(this)
-        listView.visibility = View.VISIBLE
+        listView.visibility = View.INVISIBLE
 
-        val tabLayout = findViewById<TableLayout>(R.id.TabLayout)
+
+
+        /*tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+        })*/
+
     }
 
-    override fun onPause() {
-        super.onPause()
-    }
 
-    override fun onStop() {
-        super.onStop()
-    }
 
 }
