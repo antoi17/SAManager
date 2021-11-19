9dd6405578208fe74bea206bdd4b82da9a09f2f9
antoi17 <antoine.bonin986@gmail.com>
2021-11-19 10:50:32 +0100

    (N: floating button)

--- app/src/main/java/com/example/samanager/MainActivity.kt
+++ app/src/main/java/com/example/samanager/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
 
@@ -20,29 +21,68 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listView = findViewById<ListView>(R.id.serie_list_view)
-        val tabs = findViewById<TabLayout>(R.id.Tabs)
-        val serietab = findViewById<TabItem>(R.id.serieTab)
-        val scantab = findViewById<TabItem>(R.id.scanTab)
-
         listView.adapter = DataAdapter(this)
         listView.visibility = View.INVISIBLE
 
+        val tabs = findViewById<TabLayout>(R.id.Tabs)
+        val serietab = findViewById<TabItem>(R.id.serieTab)
+        val scantab = findViewById<TabItem>(R.id.scanTab)
+        val txt = findViewById<TextView>(R.id.textView)
+
+        val floattingButton = findViewById<FloatingActionButton>(R.id.floating_action_button_add)
+
+        floattingButton.setOnClickListener{
+            txt.text = "button triggerd"
+        }
 
 
-        /*tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
+                if (tab != null) {
+                    txt.text = tab.text
+                    when {
+                        tab.text!!.equals("Séries") -> {
+                            txt.text = tab.text
+                            listView.visibility = View.VISIBLE
+                        }
+                        tab.text!!.equals("Scans") -> {
+                            txt.text = "Scans"
+                        }
+                        else -> {
+                            txt.text = "Erreur"
+                        }
+                    }
+                }
+                else {
+                    txt.text = "Erreur"
+                }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
+                if (tab != null) {
+                    txt.text = tab.text
+                    when {
+                        tab.text!!.equals("Séries") -> {
+                            listView.visibility = View.INVISIBLE
+                        }
+                        tab.text!!.equals("Scans") -> {
+                            txt.text = "Scans"
+                        }
+                        else -> {
+                            txt.text = "Erreur"
+                        }
+                    }
+                }
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
+                if (tab != null) {
+                    txt.text = tab.text
+                }
             }
 
-        })*/
+        })
 
     }
 
